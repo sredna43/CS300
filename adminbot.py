bc5376b0f6ac1d2fda873d1356c01939405f3d70
Anders <olson16@stolaf.edu>
2018-02-23 16:47:18 -0600

    updated adminbot.py

--- adminbot.py
+++ adminbot.py
@@ -1,1 +1,27 @@
 ''' Admin Bot for CloudBot project '''
+
+import sqlite3 as sq
+import sys
+import os.path
+
+''' The basic idea is to use the terminal window to
+	quickly and easily add answers to cloudbot's
+	database of answers. (answers.db)           '''
+	
+#Query the database
+if os.path.isfile("answers.db"):
+	conn = sq.connect("answers.db")
+	c = conn.cursor()
+else:
+	print("Error: No matching database")
+	sys.exit(1)
+answers = c.execute("SELECT * FROM unknowns WHERE has_answer=0")
+for row in answers:
+	print("Question: " + row[1])
+	answer = intput("Answer: ")
+	c.execute("UPDATE unknowns set admin_answer=? WHERE question=?",(answer,row[1]))
+	conn.commit()
+	
+conn.close()
+	
+	
